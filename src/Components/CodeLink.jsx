a641d22ec3193a62df386a256780cd86c9632064
oleob <ole.barsch@gmail.com>
2017-12-08 10:53:21 +0100

    Add Link to github code

---
+++ src/Components/CodeLink.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Icon from '../Icons/code.svg';
+
+const CodeLink = () => (
+  <a className="codeLink" href="https://github.com/oleob/larssyversen.no">
+    <img alt="code icon" className="codeIcon" src={Icon} />
+  </a>
+);
+
+export default CodeLink;
